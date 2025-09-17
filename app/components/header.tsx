d737d5ab5fb3109967ddf9693fc4b983440eee85
marcelofelixassis <marcelofelix.af@gmail.com>
2025-09-16 21:50:13 -0300

    chore: header image link to home

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -3,12 +3,15 @@ import { Card, CardContent } from "./ui/card";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
 import SidebarSheet from "./sidebar-sheet";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <Card className="rounded-none">
       <CardContent className="flex flex-row items-center justify-between p-5">
-        <Image alt="FSW Barber" src="/logo.svg" height={18} width={120} />
+        <Link href={"/"}>
+          <Image alt="FSW Barber" src="/logo.svg" height={18} width={120} />
+        </Link>
         <SidebarSheet>
           <Button size="icon" variant="outline">
             <MenuIcon />
